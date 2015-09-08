0ed46d96602cbcc9256cc155ae0cb21e41a997c8
Steven Asher <steve.asher@gmail.com>
2015-09-08 08:10:29 -0700

    Problem 2

---
+++ colors.js
@@ -0,0 +1,9 @@
+
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+// A method to assign the correct color names to the correct <span> elements
+var updateColors = function(colors) {
+
+};
+
+updateColors();
