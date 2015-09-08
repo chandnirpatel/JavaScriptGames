5ac1eb1913b63dcac479374e665f134a918141d7
Chandni <chandnir.patel@gmail.com>
2015-09-08 14:35:57 -0700

    listing colors in order on webpage

--- colors.js
+++ colors.js
@@ -3,7 +3,11 @@ var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 // A method to assign the correct color names to the correct <span> elements
 var updateColors = function(colors) {
-
+	for (var i = 0; i < colors.length; i++) {
+		console.log(colors[i]);
+		var outputElement = document.getElementById('color'+(i+1));
+    	outputElement.innerHTML = colors[i];
+	};
 };
 
-updateColors();
+updateColors(colors);
