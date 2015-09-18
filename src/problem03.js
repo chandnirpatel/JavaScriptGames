829087f1922e7a95e47a9dbec8a8ea057538b3f4
Chandni <chandnir.patel@gmail.com>
2015-09-17 22:43:29 -0700

    add comments for steve in problem 3

--- src/problem03.js
+++ src/problem03.js
@@ -1,1 +1,7 @@
 //Why is there no problem 3?  :)  More easy problems please!
+// Can you teach me how to add html into different parts of everything.html using JS only?
+// I would like to add the comment Steven is awesome after problem two's answer
+
+var answerProblem03 = function() {
+	console.log("Steve is awesome!");
+}
