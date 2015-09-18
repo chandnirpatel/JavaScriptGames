454826f926ea6882c6c698a9f50a8a4f7520d7c0
Chandni <chandnir.patel@gmail.com>
2015-09-17 22:42:01 -0700

    cleaning out console logs

--- src/problem02.js
+++ src/problem02.js
@@ -7,7 +7,6 @@ var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 // A method to assign the correct color names to the correct <span> elements
 var updateColors = function(colors) {
 	for (var i = 0; i < colors.length; i++) {
-		console.log(colors[i]);
 		var outputElement = document.getElementById('color'+(i+1));
     	outputElement.innerHTML = colors[i];
 	};
