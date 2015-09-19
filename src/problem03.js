c4bc873f71fe060ed666f64b675e0d5eb3f8a777
Steven Asher <steve.asher@gmail.com>
2015-09-18 19:07:07 -0700

    New problem 5.

--- src/problem03.js
+++ src/problem03.js
@@ -1,7 +1,10 @@
-//Why is there no problem 3?  :)  More easy problems please!
+// Chandni: Why is there no problem 3?  :)  More easy problems please!
 // Can you teach me how to add html into different parts of everything.html using JS only?
 // I would like to add the comment Steven is awesome after problem two's answer
 
+// Steven: Notice how I classes to the HTML to find the correct place to insert this answer.
+
 var answerProblem03 = function() {
-	console.log("Steve is awesome!");
+	var answerP2 = document.querySelector('.answer.p2');
+	answerP2.insertAdjacentHTML('afterend', '<h3>Problem 2.5:</h3><p>Steven is awesome.</p>');
 }
---
+++ test/problem5.spec.js
@@ -0,0 +1,31 @@
+// Let's write a class for adding new quetions and answers to the page
+describe('Question Adder', function() {
+  var questioner;
+
+  beforeEach(function() {
+    document.querySelector('body').innerHTML = '<h3>Problem 1</h3><p class="answer">the first answer.</p>';
+    questioner = new Questioner();
+  });
+
+  it('should add a question to the page after the first one', function() {
+      expect(document.querySelectorAll('.answer').length).toBe(1);
+
+      var theAnswer = 'To get to the other side.';
+      questioner.addQuestion(2, 'Why did the chicken cross the road?', theAnswer);
+
+      expect(document.querySelectorAll('.answer').length).toBe(2);
+      expect(document.querySelectorAll('.answer')[1].innerHTML).toBe(theAnswer);
+  });
+
+  it('should add two questions to the page', function() {
+      expect(document.querySelectorAll('.answer').length).toBe(1);
+
+      questioner.addQuestion(2, 'Why did the chicken cross the road?', 'To get to the other side.');
+
+      var theAnswer3 = "It's a great little number, but you've probably never heard of it.";
+      questioner.addQuestion(3, "How many hipster's does it take to screw in a lightbulb?", theAnswer3);
+
+      expect(document.querySelectorAll('.answer').length).toBe(3);
+      expect(document.querySelectorAll('.answer')[2].innerHTML).toBe(theAnswer3);
+  });
+});
