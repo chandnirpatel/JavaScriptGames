a95551a55410c538656e94794f0d9d626aa09c9e
Chandni <chandnir.patel@gmail.com>
2015-09-24 13:58:06 -1000

    add first word selector to complimentor

--- src/complimentor.js
+++ src/complimentor.js
@@ -1,7 +1,10 @@
-var Complimentor = function() {
+var Complimentor = function(sentences) {
 	return {
 		splitSentence: function(sentence){
 			return sentence.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g,'').split(' ');
+		},
+		chooseFirst: function(){
+			return this.splitSentence(sentences[0])[0];
 		}
 	};
 }
