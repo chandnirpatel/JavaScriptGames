1dbf3ae5cf8d109571ee2e0ee745b3e76a80bb7d
Chandni <chandnir.patel@gmail.com>
2015-09-27 08:24:19 -1000

    updated tests to pass when we clear body before each test

--- src/Questioner.js
+++ src/Questioner.js
@@ -2,15 +2,18 @@ var Questioner = function() {
 	return {
 		addQuestion: function(num, question, answer){
 			var answers = document.querySelectorAll('.answer');
-			if (answers.length == 0) {
+			if (answers.length === 0) {
 				// Q: should I handle if a body doesn't exist?
 				// A: In most regards Javascript code usually assumes a valid HTML document, which must always have a body element.
-				var lastAnswer = document.body;
+				var questionHeader = document.createElement("h3");
+				var questionHeaderText = document.createTextNode("Problem " +num);
+				questionHeader.appendChild(questionHeaderText);
+				document.body.appendChild(questionHeader);
+				questionHeader.insertAdjacentHTML('afterend', '<p class="question">'+question+'</p><p class="answer">'+answer+'</p>');
 			} else {
-				var lastAnswer = answers[answers.length-1];
+				answers[answers.length-1].insertAdjacentHTML('afterend', '<h3>Problem '+num+'</h3><p class="question">'+question+'</p><p class="answer">'+answer+'</p>');
 			};
 
-			lastAnswer.insertAdjacentHTML('afterend', '<h3>Problem '+num+'</h3><p class="question">'+question+'</p><p class="answer">'+answer+'</p>');
 		}
 	};
 }
--- test/problem5.spec.js
+++ test/problem5.spec.js
@@ -27,23 +27,27 @@ describe('Question Adder', function() {
   });
 
   it('should add a question to the page after the first one', function() {
-      expect(document.querySelectorAll('.answer').length).toBe(1);
-      expect(document.querySelectorAll('.question').length).toBe(1);
+      expect(document.querySelectorAll('.answer').length).toBe(0);
+      expect(document.querySelectorAll('.question').length).toBe(0);
+      expect(document.querySelectorAll('body').length).toBe(1);
+
 
       var theQuestion = 'Why did the chicken cross the road?'
       var theAnswer = 'To get to the other side.';
       questioner.addQuestion(2, theQuestion, theAnswer);
 
-      expect(document.querySelectorAll('.answer').length).toBe(2);
-      expect(document.querySelectorAll('.question').length).toBe(2);
-      expect(document.querySelectorAll('.answer')[1].innerHTML).toBe(theAnswer);
-      expect(document.querySelectorAll('.question')[1].innerHTML).toBe(theQuestion);
-      expect(document.querySelectorAll('h3')[1].innerHTML).toBe('Problem 2');
+      expect(document.querySelectorAll('.answer').length).toBe(1);
+      expect(document.querySelectorAll('.question').length).toBe(1);
+      expect(document.querySelectorAll('.answer')[0].innerHTML).toBe(theAnswer);
+      expect(document.querySelectorAll('.question')[0].innerHTML).toBe(theQuestion);
+      expect(document.querySelectorAll('h3')[0].innerHTML).toBe('Problem 2');
   });
 
   it('should add two questions to the page', function() {
-      expect(document.querySelectorAll('.answer').length).toBe(2);
-      expect(document.querySelectorAll('.question').length).toBe(2);
+      expect(document.querySelectorAll('.answer').length).toBe(0);
+      expect(document.querySelectorAll('.question').length).toBe(0);
+      expect(document.querySelectorAll('body').length).toBe(1);
+
 
       questioner.addQuestion(2, 'Why did the chicken cross the road?', 'To get to the other side.');
 
@@ -51,10 +55,10 @@ describe('Question Adder', function() {
       var theAnswer3 = "It's a great little number, but you've probably never heard of it.";
       questioner.addQuestion(3, "How many hipster's does it take to screw in a lightbulb?", theAnswer3);
 
-      expect(document.querySelectorAll('.answer').length).toBe(4);
-      expect(document.querySelectorAll('.question').length).toBe(4);
-      expect(document.querySelectorAll('.answer')[3].innerHTML).toBe(theAnswer3);
-      expect(document.querySelectorAll('.question')[3].innerHTML).toBe(theQuestion3);
-      expect(document.querySelectorAll('h3')[3].innerHTML).toBe('Problem 3');
+      expect(document.querySelectorAll('.answer').length).toBe(2);
+      expect(document.querySelectorAll('.question').length).toBe(2);
+      expect(document.querySelectorAll('.answer')[1].innerHTML).toBe(theAnswer3);
+      expect(document.querySelectorAll('.question')[1].innerHTML).toBe(theQuestion3);
+      expect(document.querySelectorAll('h3')[1].innerHTML).toBe('Problem 3');
   });
 });
