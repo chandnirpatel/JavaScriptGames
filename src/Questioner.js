ab4942faa5cc91680dcf71a54c4d2afcf81cb323
Chandni <chandnir.patel@gmail.com>
2015-09-27 08:27:12 -1000

    removing comments

--- src/Questioner.js
+++ src/Questioner.js
@@ -3,8 +3,6 @@ var Questioner = function() {
 		addQuestion: function(num, question, answer){
 			var answers = document.querySelectorAll('.answer');
 			if (answers.length === 0) {
-				// Q: should I handle if a body doesn't exist?
-				// A: In most regards Javascript code usually assumes a valid HTML document, which must always have a body element.
 				var questionHeader = document.createElement("h3");
 				var questionHeaderText = document.createTextNode("Problem " +num);
 				questionHeader.appendChild(questionHeaderText);
